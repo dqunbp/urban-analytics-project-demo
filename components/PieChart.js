2baaa0f2f14490e3526fd86cc6d70f48608f0e5f
Tigran Grigoryan <dqunbp@gmail.com>
2018-05-07 18:12:44 +0300

    add chart forward ref

--- components/PieChart.js
+++ components/PieChart.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import C3Chart from 'react-c3js'
 
-export const PieChart = ({ columns, colors, onClick, onMouseOver, onMouseOut }) => (
-    <C3Chart
-        data={{
-            columns: columns,
-            colors: colors || {},
-            type: 'pie',
-            onclick: onClick,
-            onmouseover: onMouseOver,
-            onmouseout: onMouseOut,
-        }}
-        legend={{ show: false }}
-    />
+export const PieChart = React.forwardRef(
+    ({ columns, colors, onClick, onMouseOver, onMouseOut }, ref) => (
+        <C3Chart
+            ref={ref}
+            data={{
+                columns: columns,
+                colors: colors || {},
+                type: 'pie',
+                onclick: onClick,
+                onmouseover: onMouseOver,
+                onmouseout: onMouseOut,
+            }}
+            legend={{ show: false }}
+        />
+    )
 )
 
 export default PieChart
