e65e60f065bac414544cce95560fa01befc533c6
Tigran Grigoryan <dqunbp@gmail.com>
2018-05-07 14:59:57 +0300

    add app structure template

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom"
 import 'normalize.css'
 
-class HelloMessage extends React.Component {
+class App extends React.Component {
   render() {
     return (
-        <div>Hello {this.props.name}</div>
+        <div className="app">
+            <div className="sibebar">
+                Sidebar
+            </div>
+            <div className="map">
+                Map
+            </div>
+        </div>
     )
   }
 }
 
 var mountNode = document.getElementById("app")
-ReactDOM.render(<HelloMessage name="MR!" />, mountNode)
+ReactDOM.render(<App />, mountNode)
