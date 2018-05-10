13163ed9a81568f2a915da89140ff5b85defc25a
Tigran Grigoryan <dqunbp@gmail.com>
2018-05-10 18:49:32 +0300

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' })
+}
+
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  const CSSExtract = new ExtractTextPlugin('styles.css')
+  const defineEnvVariables = new webpack.DefinePlugin({
+    'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:8080')
+  })
+
+  return {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }]
+    },
+    plugins: [
+      defineEnvVariables,
+      CSSExtract,
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/',
+      port: 3000,
+    }
+  }
+}
